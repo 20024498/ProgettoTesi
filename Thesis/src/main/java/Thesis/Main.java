f081c760648c50defebc41a04cfec83570ebcc7a
andrea <andrea@andrea-VirtualBox>
2020-05-17 22:28:20 +0200

    Creazione rete bayesiana

--- Thesis/src/main/java/Thesis/Main.java
+++ Thesis/src/main/java/Thesis/Main.java
@@ -1,14 +1,116 @@
 package Thesis;
 
+import java.awt.Color;
+
 import smile.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		//licenza
 		licenseInit();
+		
+		//Inizializzazione rete
 		Network net = new Network();
-		System.out.println("FUNZIONA");
-
+		net.readFile("net/rete.xdsl");
+		
+		//clear iniziale
+		StringBuilder code = new StringBuilder();
+		code.append("clear \n\n");
+		
+		//variabili nascoste
+		code.append("h_states = {");
+		for (int h = net.getFirstNode(); h >= 0; h = net.getNextNode(h)) {
+			
+			if(net.getNodeBgColor(h).equals(new Color(229,246,247))) {
+				code.append(net.getNodeName(h));
+				code.append(", ");
+			}
+		}
+		if(code.length()>=2) {
+			code.deleteCharAt(code.length()-1);
+			code.deleteCharAt(code.length()-1);
+		}
+		code.append("};\n");
+		
+		//variabili osservabili
+		code.append("obs = {");
+		for (int h = net.getFirstNode(); h >= 0; h = net.getNextNode(h)) {
+			
+			if(!net.getNodeBgColor(h).equals(new Color(229,246,247))) {
+				code.append(net.getNodeName(h));
+				code.append(", ");
+			}
+		}
+		if(code.length()>=2) {
+			code.deleteCharAt(code.length()-1);
+			code.deleteCharAt(code.length()-1);
+		}
+		code.append("};\n");
+		
+		//insieme dei nomi
+		code.append("names=[h_states, obs];\n\n");
+		
+		//numero di nodi
+		code.append("n=length(names);\n\n");
+		
+		//archi intraslice
+		code.append("intrac={");
+		for (int h = net.getFirstNode(); h >= 0; h = net.getNextNode(h)) {
+			int[] children;
+			if((children = net.getChildren(h)).length>0) 
+				for(Integer i : children) {
+					code.append("'"+net.getNodeName(h)+"'");
+					code.append(", ");
+					code.append("'"+net.getNodeName(i)+"'");
+					code.append(";\n");
+				}	
+		}
+		if(code.length()>=2) {
+			code.deleteCharAt(code.length()-1);
+			code.deleteCharAt(code.length()-1);
+		}
+		code.append("};\n\n");
+		
+		//matrice adiacenza archi intraslice
+		code.append("[intra, names] = mk_adj_mat(intrac, names, 1);\n\n");
+		
+		//archi interslice
+		code.append("interc={");
+		for (int h = net.getFirstNode(); h >= 0; h = net.getNextNode(h)) {
+			TemporalInfo[] tChildren;
+			if((tChildren = net.getTemporalChildren(h)).length>0) 
+				for(TemporalInfo i : tChildren) {
+					code.append("'"+net.getNodeName(h)+"'");
+					code.append(", ");
+					code.append("'"+net.getNodeName(i.handle)+"'");
+					code.append(";\n");
+				}
+			
+		}
+		if(code.length()>=2) {
+			code.deleteCharAt(code.length()-1);
+			code.deleteCharAt(code.length()-1);
+		}
+		code.append("};\n\n");
+		
+		//matrice adiacenza archi interslice
+		code.append("inter = mk_adj_mat(interc, names, 0);\n\n");
+		
+		//numero di stati i-esima variabile
+		code.append("ns = [");
+		for (int h = net.getFirstNode(); h >= 0; h = net.getNextNode(h)) 
+			code.append(net.getOutcomeCount(h)+" ");
+		if(code.length()>=1)
+			code.deleteCharAt(code.length()-1);
+		code.append("];\n\n");
+		
+		//creazione rete bayesiana
+		code.append("bnet = mk_dbn(intra, inter, ns, 'names', names);\n\n");
+		
+		System.out.println(code.toString());	
+			
 	}
 
 	private static License licenseInit() {
